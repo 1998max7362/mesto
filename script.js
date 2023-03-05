19c62d1fd4f35dc8c95733223f0f87df7895e401
Max <1998max7362@mail.ru>
2023-03-05 04:29:40 +0300

    finished

---
+++ script.js
@@ -0,0 +1,49 @@
+// Находим форму в DOM
+const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__container');
+// Находим поля формы в DOM
+const nameInput = formElement.querySelector('#Name');
+const jobInput = formElement.querySelector('#Job');
+const closeButton = formElement.querySelector('.popup__close-button');
+const saveButton = formElement.querySelector('.popup__save-button');
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+const editButton = document.querySelector('.profile__editButton');
+const addButton = document.querySelector('.profile__addButton');
+
+
+const openPopup = ()=>{
+    popup.classList.add('popup_opened')
+    nameInput.value = profileName.textContent
+    jobInput.value = profileJob.textContent
+}
+
+const closePopup = ()=>{
+    popup.classList.remove('popup_opened')
+}
+
+editButton.addEventListener('click',openPopup)
+closeButton.addEventListener('click',closePopup)
+
+
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    // Выберите элементы, куда должны быть вставлены значения полей
+    // Вставьте новые значения с помощью textConten
+    profileName.textContent = nameInput.value
+    profileJob.textContent = nameInput.value
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit); 

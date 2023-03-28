b3dac3b9f1b3db6189cc4d61ced7eacf3564ca32
Max <1998max7362@mail.ru>
2023-03-28 13:31:20 +0300

    bugfix: закрытие попапа на Esc теперь назначается и удаляется при открытии\закрытии

--- scripts/popups/popup.js
+++ scripts/popups/popup.js
@@ -1,10 +1,14 @@
+
+const closePopupByKey = (key, popup) => key === 'Escape'? closePopup(popup):false
+
 const closePopup = (popup) => {
   popup.classList.remove('popup_opened')
+  document.removeEventListener('keydown', evt => closePopupByKey(evt.key, popup))
 }
 
 const openPopup = (popup) => {
   popup.classList.add('popup_opened')
-  document.addEventListener('keydown', evt => evt.key==='Escape'? closePopup(popup):false, { once: true })
+  document.addEventListener('keydown',  evt => closePopupByKey(evt.key, popup))
 }
 
 
